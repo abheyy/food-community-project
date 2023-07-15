76f87d5d68732d7615f14ffda112f10d7083209d
abheyy <iamabhaydhasmana@gmail.com>
2023-07-15 11:35:44 +0530

    a

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div>
+    
+
+      <footer className="footer">ABHAY's Foodary © 2023</footer>
+      
+      </div>
+    );
+  }
+  export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,40 @@
+export const my_recipe = {
+  recipe_1: {
+    name: 'Chirashi Don',
+    preparation_time: '30 minutes',
+    servings: '2',
+    img:'/img/recipe1.jpg',
+    ingredients: [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    instructions: [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+ 
+  },
+  recipe_2: {
+    name: 'Tea',
+    preparation_time: '5 minutes',
+    servings: '1',
+    img:'/img/recipe2.jpg',
+    ingredients: [
+      '1 cup water',
+      '1 spoon tealeaves',
+      '4 spoon sugar',
+      '10g of ginger',
+      '100ml milk',
+    ],
+    instructions: [
+      'Boil the water',
+      'Add tealeaves , sugar and ginger in it',
+      'After it start to boil, add milk',
+      'Serve tea on a cup',
+    ],
+ 
+  }
+}
